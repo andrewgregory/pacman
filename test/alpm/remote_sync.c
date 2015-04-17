1110a3822ddd1eb662caef1a8bf02b387c41cc95
Andrew Gregory <andrew.gregory.8@gmail.com>
2015-04-17 15:07:10 -0400

    add ASSERTC

--- test/alpm/remote_sync.c
+++ test/alpm/remote_sync.c
@@ -1,38 +1,54 @@
+#ifndef HAVE_PTHREAD
+
+#include "tap.h"
 #include "pactest.c"
 #include "ptserve.c"
-#include "tap.h"
 
 /* sync a package from a "remote" repository */
 
-#define ASSERT(x) if(!(x)) { tap_bail("ASSERT FAILED %s line %d: '%s'", __FILE__, __LINE__, #x); exit(1); }
+#define ASSERTC(x) \
+    if(!(x)) { \
+        tap_bail("ASSERT FAILED %s line %d: '%s'", __FILE__, __LINE__, #x); \
+        cleanup(); \
+        exit(1); \
+    }
+
+ptserve_t *ptserve = NULL;
+pt_env_t *pt = NULL;
+pt_pkg_t *pkg;
+pt_db_t *db;
+alpm_pkg_t *lpkg;
+alpm_db_t *adb;
+alpm_list_t *data = NULL;
+
+void cleanup(void) {
+    pt_cleanup(pt);
+    ptserve_free(ptserve);
+}
+
+int pt_can_chroot(void) {
+    return chroot("/") == 0;
+}
 
 int main(void) {
-    pt_env_t *pt;
-    pt_pkg_t *pkg;
-    pt_db_t *db;
-    alpm_pkg_t *lpkg;
-    alpm_db_t *adb;
-    alpm_list_t *data = NULL;
-    ptserve_t *ptserve;
+    ASSERTC(pt = pt_new(NULL));
+    ASSERTC(pt_mkdirat(pt->rootfd, 0755, "srv") == 0);
 
-    ASSERT(pt = pt_new(NULL));
-    ASSERT(pt_mkdirat(pt->rootfd, 0755, "srv") == 0);
+    ASSERTC(pkg = pt_pkg_new(pt, "foo", "1-1"));
+    ASSERTC((free(pkg->filename), pkg->filename = strdup("foo.pkg.tar")));
+    ASSERTC(pt_pkg_writeat(pt->rootfd, "srv/foo.pkg.tar", pkg) == 0);
 
-    ASSERT(pkg = pt_pkg_new(pt, "foo", "1-1"));
-    ASSERT(pkg->filename = strdup("foo.pkg.tar"));
-    ASSERT(pt_pkg_writeat(pt->rootfd, "srv/foo.pkg.tar", pkg) == 0);
+    ASSERTC(db = pt_db_new(pt, "sync"));
+    ASSERTC(pt_db_add_pkg(db, pkg));
+    ASSERTC(pt_install_db(pt, db) == 0);
 
-    ASSERT(db = pt_db_new(pt, "sync"));
-    ASSERT(pt_db_add_pkg(db, pkg));
-    ASSERT(pt_install_db(pt, db) == 0);
+    ASSERTC(ptserve = ptserve_serve_dirat(pt->rootfd, "srv/"));
 
-    ASSERT(ptserve = ptserve_serve_dirat(pt->rootfd, "srv/"));
-    
-    ASSERT(pt_initialize(pt, NULL));
-    ASSERT(alpm_option_add_cachedir(pt->handle, pt_path(pt, "tmp")) == 0);
-    ASSERT(adb = alpm_register_syncdb(pt->handle, "sync", 0));
-    ASSERT(alpm_db_add_server(adb, ptserve->url) == 0);
-    ASSERT(lpkg = pt_alpm_get_pkg(pt->handle, "sync/foo"));
+    ASSERTC(pt_initialize(pt, NULL));
+    ASSERTC(alpm_option_add_cachedir(pt->handle, pt_path(pt, "tmp")) == 0);
+    ASSERTC(adb = alpm_register_syncdb(pt->handle, "sync", 0));
+    ASSERTC(alpm_db_add_server(adb, ptserve->url) == 0);
+    ASSERTC(lpkg = pt_alpm_get_pkg(pt->handle, "sync/foo"));
 
     tap_plan(6);
     tap_is_int(alpm_trans_init(pt->handle, 0), 0, "alpm_trans_init");
@@ -42,7 +58,17 @@ int main(void) {
     tap_is_int(alpm_trans_release(pt->handle), 0, "alpm_trans_release");
     tap_is_str(pt_test_pkg_version(pt, "foo"), "1-1", "foo installed");
 
-    pt_cleanup(pt);
-    ptserve_free(ptserve);
+    cleanup();
     return tap_finish();
 }
+
+#else
+
+#include "tap.h"
+
+int main(void) {
+    tap_skip_all("pthread is required for ptserve");
+    return 0;
+}
+
+#endif
